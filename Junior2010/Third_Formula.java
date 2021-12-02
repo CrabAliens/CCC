fdc51b8eff4da34fbe68bbdc5219040c6dbbd794
qwerty <qwerty.uiop@asdfg.hjkl>
2021-12-01 23:09:17 -0500

    Third_Code_Check

--- Junior2010/Third_Formula.java
+++ Junior2010/Third_Formula.java
@@ -4,33 +4,80 @@ import java.util.*;
 import java.io.*;
 
 public class Third_Formula
-{
-  int A = 0;
-  int B = 0;
-  
-  public void formula( int command, String var, int amount )
+{  
+
+  public void formula( int command, int X, int Y, int n, boolean end )
   {
-    if( command == 1 )
+    switch( command )
     {
-      A = amount
+      case 1: X = n;
+              break;
+      case 2: System.out.println( X );
+              break;
+      case 3: X += Y;
+              break;
+      case 4: X *= Y;
+              break;
+      case 5: X -= Y;
+              break;
+      case 6: X = ( int )( X/Y );
+              break;
+      case 7: end = true;
+              break;
     }
-    else if( command == 2 )
+  }
+  public static void main( String args[] )throws IOException
+  {
+    Scanner scanner = new Scanner( System.in );
+    
+    boolean end = false;
+    
+    int command;
+    int n = 0;
+    int X = 0;
+    int Y = 0;
+
+    String third;
+    
+    while( end == false )
     {
+      int A = X;
+      int B = Y;
+      System.out.println( "Input your code" );
+      
+      command = Integer.parseInt( scanner.next() );
+      
+      if( scanner.next().equals("A") )
+      {
+        X = A;
+      }
+      else
+      {
+        X = B;
+      }
+      
+      third = scanner.next();
+      
+      if( third.equals("A") )
+      {
+        Y = A;
+      }
+      else if( third.equals("B") )
+      {
+        Y = B;
+      }
+      else
+      {    
+        n = Integer.parseInt( third );
+      }
+      
+      Third_Formula formula = new Third_Formula();
+      
+      formula.formula( command, X, Y, n, end );
+      
+      scanner.nextLine();
+      
     }
-    else if( command == 3 )
-    {
-    }
-    else if( command == 4 )
-    {
-    }
-    else if( command == 5 )
-    {
-    }
-    else if( command == 6 )
-    {
-    }
-    else
-    {
-    }
+    
   }
 }
