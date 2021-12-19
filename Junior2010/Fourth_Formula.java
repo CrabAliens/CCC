4e2624ac724f2db7d9ea55471abb9947a64ab769
qwerty <qwerty.uiop@asdfg.hjkl>
2021-12-18 22:11:46 -0500

    FourthCODDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDE

--- Junior2010/Fourth_Formula.java
+++ Junior2010/Fourth_Formula.java
@@ -11,25 +11,60 @@ public class Fourth_Formula
     
     ArrayList<Integer> pattern = new ArrayList<Integer>();
     
+    boolean check = true;
+    
     for( int i = 2; i < temps.size(); i++ )
       gaps.add( temps.get(i) - temps.get(i-1) );
     
-    for( int j = 0; j < gaps.size(); j++ )        
+    for( int j = 1; j < gaps.size(); j++ )
     {
-      if( gaps.get( j ) == gaps.get( 0 ) )
+      if( gaps.get(j) == gaps.get(0) )
       {
-        for( int k = 0; k < j; k++ )
-        {
-          pattern.add( gaps.get( k ) );
+        pattern.addAll( gaps.subList( 0, j - 1 ) );
+        
+        for( int l = 0; l <= gaps.size() - 3; l = l + 3 )
+        {  
+          check = true;
+          if( gaps.subList(j + l, j + l + pattern.size() - 1 ) != pattern )
+          {
+            check = false;
+            break;
+          }
         }
+        if( check = true )
+        {
+          for( int k = 0; k <= gaps.size() % pattern.size(); k++ )
+          {
+            if( gaps.subList( gaps.size() - pattern.size(), gaps.size() - pattern.size() + ( gaps.size() % pattern.size() ) ) == gaps.subList( 0, gaps.size() % pattern.size() ) )
+            {
+              return pattern.size();
+            }
+          }
+        }      
       }
     }
+    return gaps.size();
   }
       
   
   public static void main( String args[] )throws IOException
   {
     ArrayList< Integer > list = new ArrayList<>();
+    
+    System.out.println( "Input your list" );
+      
+    Scanner scanner = new Scanner( System.in );
+      
+    int size = scanner.nextInt();
+      
+    for( int m = 1; m <= size; m++ )
+      list.add( scanner.nextInt() );
+      
+    System.out.println( list );
+    
+    Fourth_Formula code = new Fourth_Formula();
+    
+    System.out.println( code.formula( list ) );
   }
     
 }
