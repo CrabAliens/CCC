059494197ac00fe36733ff418915c8718932039e
qwerty <qwerty.uiop@asdfg.hjkl>
2022-02-04 15:01:11 -0500

    YAYYYYYY

--- Junior2011/Fourth.java
+++ Junior2011/Fourth.java
@@ -5,8 +5,7 @@ import java.io.*;
 public class Fourth
 {
   ArrayList<Integer> map = new ArrayList<Integer>();
-  
-  public boolean end = false;
+  boolean end = false;
   
   public void addMap( int x, int y )
   {
@@ -14,106 +13,76 @@ public class Fourth
     map.add( y );
   }
   
-  public String formula( String dir, int amount )
+  public int move_and_check( int l_r, int u_d, int amount, ArrayList<Integer> map )
   {
-    if( dir.equals( "l" ) )
+    int past_map_size = map.size();
+    for( int i = 1; i <= amount; i++ )
     {
-      map.add( map.get( map.size() - 2 ) + amount );
-      map.add( map.get( map.size() - 1 ) );
+      map.add( map.get( map.size() - 2 ) + ( 1 * l_r ) );
+      map.add( map.get( map.size() - 2 ) + ( 1 * u_d ) );
     }
-    for( int i = 0; i < map.size() - 2; i = i + 2 )
+    if( l_r == 1 || l_r == -1 )
     {
-      if( map.get( i ) == map.get( map.size() - 2 ) )
+      for( int j = 0; j < map.size() - 2; j += 2 )
       {
-        end = true;
-        return ( Integer.toString( map.get( map.size() - 2 ) ) + ", " + Integer.toString( map.get( map.size() - 1 ) ) + " DANGER" );
+        if( map.get( j ) == map.get( map.size() - 2 ) && map.get( j + 1 ) == map.get( map.size() - 1 ) )
+          return -1;
       }
-      else
-      {
-          end = false;
-          return ( Integer.toString( map.get( map.size() - 2 ) ) + ", " + Integer.toString( map.get( map.size() - 1 ) ) + " safe" );
-      }
+      return 1;
     }
-    if( dir.equals( "r" ) )
+    else
     {
-      System.out.println( amount );
-      System.out.println( map.get( map.size() - 2 ));      
-      map.add( map.get( map.size() - 2 ) - amount );
-      map.add( map.get( map.size() - 1 ) );
-    }
-    for( int i = 0; i < map.size() - 2; i = i + 2 )
-    {
-      if( map.get( i ) == map.get( map.size() - 2 ) )
+      for( int j = 0; j < map.size() - 2; j += 2 )
       {
-        end = true;
-        return ( Integer.toString( map.get( map.size() - 2 ) ) + ", " + Integer.toString( map.get( map.size() - 1 ) ) + " DANGER" );
+        if( map.get( j ) == map.get( map.size() - 2 ) && map.get( j + 1 ) == map.get( map.size() - 1 ) )
+          return -1;
       }
-      else
-      {
-          end = false;
-          return ( Integer.toString( map.get( map.size() - 2 ) ) + ", " + Integer.toString( map.get( map.size() - 1 ) ) + " safe" );
-      }
+      return 1;
     }
-    if( dir.equals( "u" ) )
-    {
-      map.add( map.get( map.size() - 2 ) );
-      map.add( map.get( map.size() - 1 ) + amount );
-    }
-    for( int i = 1; i < map.size() - 1; i = i + 2 )
-    {
-      if( map.get( i ) == map.get( map.size() - 1 ) )
-      {
-        end = true;
-        return ( Integer.toString( map.get( map.size() - 2 ) ) + ", " + Integer.toString( map.get( map.size() - 1 ) ) + " DANGER" );
-      }
-      else
-      {
-          end = false;
-          return ( Integer.toString( map.get( map.size() - 2 ) ) + ", " + Integer.toString( map.get( map.size() - 1 ) ) + " safe" );
-      }
-    }
-    if( dir.equals( "d" ) )
-    {
-      map.add( map.get( map.size() - 2 ) );
-      map.add( map.get( map.size() - 1 ) - amount );
-    }
-    for( int i = 1; i < map.size() - 1; i = i + 2 )
-    {
-      if( map.get( i ) == map.get( map.size() - 1 ) )
-      {
-        end = true;
-        return ( Integer.toString( map.get( map.size() - 2 ) ) + ", " + Integer.toString( map.get( map.size() - 1 ) ) + " DANGER" );
-      }
-      else
-      {
-          end = false;
-          return ( Integer.toString( map.get( map.size() - 2 ) ) + ", " + Integer.toString( map.get( map.size() - 1 ) ) + " safe" );
-      }
-    }
-    
-    return "booboobiiibiiiiiiiiibaaaaa";
   }
-    
+  public String formula( ArrayList<Integer> map, String dir, int amount )
+  {
+    int result = 100;
+    if( dir.equals( "l" ) )
+      result = move_and_check( -1, 0, amount, map );
+    else if( dir.equals( "r" ) )
+      result = move_and_check( 1, 0, amount, map );
+    else if( dir.equals( "u" ) )
+      result = move_and_check( 0, 1, amount, map );
+    else
+      result = move_and_check( 0, -1, amount, map );
+    if( result == -1 )
+    {
+      end = true;
+      return ( Integer.toString( map.get( map.size() - 2 ) ) + "  " + Integer.toString( map.get( map.size() - 1 ) ) + "  " + "DANGER" );
+    }
+    else if( result == 1 )
+      return ( Integer.toString( map.get( map.size() - 2 ) ) + "  " + Integer.toString( map.get( map.size() - 1 ) ) + "  " + "safe" );
+    else
+      return "WRONG";
+  }
+         
+  
   public static void main( String args[] )throws IOException
   {
     Fourth code = new Fourth();
     
     code.addMap( 0, -1 );
     
-    code.formula( "d", 2 );
-    code.formula( "r", 3 );
-    code.formula( "d", 2 );
-    code.formula( "r", 2 );
-    code.formula( "u", 2 );
-    code.formula( "r", 2 );
-    code.formula( "d", 4 );
-    code.formula( "l", 8 );
-    code.formula( "u", 2 );
+    code.formula( code.map, "d", 2 );
+    code.formula( code.map, "r", 3 );
+    code.formula( code.map, "d", 2 );
+    code.formula( code.map, "r", 2 );
+    code.formula( code.map, "u", 2 );
+    code.formula( code.map, "r", 2 );
+    code.formula( code.map, "d", 4 );
+    code.formula( code.map, "l", 8 );
+    code.formula( code.map, "u", 2 );
     
     Scanner scanner = new Scanner( System.in );
           
     while( code.end == false )
-      System.out.println( code.formula( scanner.next(), scanner.nextInt() ));
+      System.out.println( code.formula( code.map, scanner.next(), scanner.nextInt() ));
   }
   
 }
